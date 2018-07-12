8b3bfcaa1add0542101dd27051bbcd807ecf927e
souvikdgreat <souvikc40@gmail.com>
2018-07-12 09:18:02 +0530

    "collection assignmnerts commit by souvik"

---
+++ CollectionProject/src/CollectionPackage/Interns.java
@@ -0,0 +1,31 @@
+package CollectionPackage;
+
+class Interns {
+    private String name;
+    private int id,score;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+}
+
