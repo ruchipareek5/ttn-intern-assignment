182849ee0614a5f8d94fa72b930ed0a398886870
@Amarjeet-Malik011 <ch.amarjeetmalik@gmail.com>
2018-07-14 22:03:47 +0530

    Stream first assignment

---
+++ amarjeet assgn/StreamAssgn/Person.java
@@ -0,0 +1,49 @@
+package StreamAssgn;
+public class Person {
+    public String name;
+    private int age;
+    enum Gender {
+        male,female
+    };
+    public int gender;
+
+
+    Person(String name, int age, Gender gender)
+    {
+        this.name = name;
+        this.age = age;
+        this.gender = gender.ordinal();
+    }
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public int getGender() {
+        return gender;
+    }
+
+    public void setGender(int gender) {
+        this.gender = gender;
+    }
+	
+    @Override
+    public String toString() {
+        return "Person{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                ", gender=" + gender +
+                '}';
+    }
+}
---
+++ amarjeet assgn/StreamAssgn/StreamDemoApp.java
@@ -0,0 +1,38 @@
+package StreamAssgn;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+public class StreamDemoApp {
+
+    public static void main(String[] args) {
+        List<Person> list = new ArrayList<>();
+        list.add(new Person("Gaurav",21,Person.Gender.male));
+        list.add(new Person("Siddarth",21,Person.Gender.male));
+        list.add(new Person("Chirag",19,Person.Gender.male));
+        list.add(new Person("Yatin",24,Person.Gender.male));
+        list.add(new Person("Mehek",20,Person.Gender.female));
+
+        Iterator e = list.iterator();
+
+        while(e.hasNext())
+        {
+            System.out.println((Person)e.next());
+        }
+
+        Stream stream = list.stream();
+        List<Person> listFemale =  stream.filter(s->s.getGender()==1).collect(Collectors.toList());
+
+        stream = list.stream();
+        list = stream.map(s -> s.setAge(s.getAge()-2)).collect(Collectors.toList());
+        e = list.iterator();
+
+        while(e.hasNext())
+        {
+            System.out.println((Person)e.next());
+        }
+
+    }
+}
