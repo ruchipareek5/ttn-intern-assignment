212c7d26f0eafc7235890d4b5334f4c8087d5162
Gaurav Gandhi <gauravgo4friends@gmail.com>
2018-07-18 23:29:36 +0530

    Assignment for JQuery and Ajax is uploaded.

---
+++ Gaurav Gandhi/18th July Assignment (Jquery+Ajax)/AjaxPractice.java
@@ -0,0 +1,39 @@
+package practice.spring.revisedSpring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class AjaxPractice {
+
+    @RequestMapping(value="/approvedfunction",method=RequestMethod.POST)
+    @ResponseBody
+    public void approved(@RequestParam String id){
+        System.out.println(id + " is approved.");
+        return ;
+    }
+
+
+    @RequestMapping(value="/rejectedfunction",method=RequestMethod.POST)
+    @ResponseBody
+    public void rejected(@RequestParam String id){
+        System.out.println(id + " is rejected.");
+        return ;
+    }
+
+
+    @RequestMapping(value="/incompletefunction",method=RequestMethod.POST)
+    @ResponseBody
+    public void Approved(@RequestParam String id){
+        System.out.println(id + " is incomplete.");
+        return ;
+    }
+    @RequestMapping("/ajax")
+    public String ajax(){
+        return "JqueryAjaxAssignment";
+    }
+}
